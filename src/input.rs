2e662efbae4c6693a8d1f8ad08a6a7e998103cb7
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-06 16:26:25 -0400

    aider: Refactored and moved the input functionality into its own lib module.
    # Context:
    USER: Please refactor and move the input functionality into its own lib module
    ASSISTANT: To refactor and move the input functionality into its own lib module, I need to know which file contains the input functionality. Could you please provide the file name and its details?
    USER: I added these *read-write* files: src/arena.rs, src/camera.rs, src/main.rs, src/player.rs
    ASSISTANT: To refactor and move the input functionality into its own lib module, we will create a new file named `input.rs` in the `src` directory. This new file will contain the `input` function currently located in `main.rs`. We will also need to modify `main.rs` to import the `input` function from the new module.
    
    Here are the changes:
    
    ```rust
    src/input.rs
    <<<<<<< ORIGINAL
    =======
    use bevy::prelude::*;
    
    pub const INPUT_UP: u8 = 1 << 0;
    pub const INPUT_DOWN: u8 = 1 << 1;
    pub const INPUT_LEFT: u8 = 1 << 2;
    pub const INPUT_RIGHT: u8 = 1 << 3;
    pub const INPUT_FIRE: u8 = 1 << 4;
    
    pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
        let mut input = 0u8;
        if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
            input |= INPUT_UP;
        }
        if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
            input |= INPUT_DOWN;
        }
        if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
            input |= INPUT_LEFT
        }
        if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
            input |= INPUT_RIGHT;
        }
        if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
            input |= INPUT_FIRE;
        }
        input
    }
    >>>>>>> UPDATED
    ```
    
    ```rust
    src/main.rs
    <<<<<<< ORIGINAL
    const INPUT_UP: u8 = 1 << 0;
    const INPUT_DOWN: u8 = 1 << 1;
    const INPUT_LEFT: u8 = 1 << 2;
    const INPUT_RIGHT: u8 = 1 << 3;
    const INPUT_FIRE: u8 = 1 << 4;
    
    fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
        let mut input = 0u8;
        if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
            input |= INPUT_UP;
        }
        if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
            input |= INPUT_DOWN;
        }
        if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
            input |= INPUT_LEFT
        }
        if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
            input |= INPUT_RIGHT;
        }
        if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
            input |= INPUT_FIRE;
        }
        input
    }
    =======
    use crate::input::input;
    >>>>>>> UPDATED
    ```
    
    Please let me know if you need further changes.

---
+++ src/input.rs
@@ -0,0 +1,27 @@
+use bevy::prelude::*;
+
+pub const INPUT_UP: u8 = 1 << 0;
+pub const INPUT_DOWN: u8 = 1 << 1;
+pub const INPUT_LEFT: u8 = 1 << 2;
+pub const INPUT_RIGHT: u8 = 1 << 3;
+pub const INPUT_FIRE: u8 = 1 << 4;
+
+pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
+    let mut input = 0u8;
+    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
+        input |= INPUT_UP;
+    }
+    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
+        input |= INPUT_DOWN;
+    }
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        input |= INPUT_LEFT
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        input |= INPUT_RIGHT;
+    }
+    if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
+        input |= INPUT_FIRE;
+    }
+    input
+}
--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,7 @@ struct GgrsConfig;
 
 const FPS: usize = 60;
 
-const INPUT_UP: u8 = 1 << 0;
-const INPUT_DOWN: u8 = 1 << 1;
-const INPUT_LEFT: u8 = 1 << 2;
-const INPUT_RIGHT: u8 = 1 << 3;
-const INPUT_FIRE: u8 = 1 << 4;
-
-fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
-    let mut input = 0u8;
-    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
-        input |= INPUT_UP;
-    }
-    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
-        input |= INPUT_DOWN;
-    }
-    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
-        input |= INPUT_LEFT
-    }
-    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
-        input |= INPUT_RIGHT;
-    }
-    if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
-        input |= INPUT_FIRE;
-    }
-    input
-}
+use crate::input::input;
 
 impl Config for GgrsConfig {
     // 4-directions + fire fits easily in a single byte
