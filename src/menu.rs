41c1bd0abb1997f4614f07048ecbf0dbc1fa6989
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-06 21:20:04 -0400

    Refactor menu setup to use a single spawn call with child entities.

--- src/menu.rs
+++ src/menu.rs
@@ -12,44 +12,64 @@ impl Plugin for MenuPlugin {
 
 fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Load the button materials
-    let button_materials = asset_server.load("assets/button_materials.png");
+    let button_materials = asset_server.load("button_materials.png");
 
     // Create a node to contain the buttons
-    let node = commands.spawn(NodeBundle {
+    commands.spawn(NodeBundle {
         style: Style {
             size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
             ..Default::default()
         },
+        background_color: BackgroundColor::from(Color::rgb(0.26, 0.26, 0.32)),
         ..Default::default()
-    }).current_entity().unwrap();
+    }).with_children(|parent| {
+        parent.spawn((ButtonBundle {
+            image: UiImage{
+                texture: button_materials.clone(),
+                flip_x: false,
+                flip_y: false,
+            },
+            style: Style {
+                size: Size::new(Val::Px(279.), Val::Px(76.)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..Default::default()
+        }, LocalGameButton));
+        parent.spawn((ButtonBundle {
+            image: UiImage{
+                texture: button_materials.clone(),
+                flip_x: false,
+                flip_y: false,
+            },
+            style: Style {
+                size: Size::new(Val::Px(279.), Val::Px(76.)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..Default::default()
+        }, OneVOneGameButton));
+        parent.spawn((ButtonBundle {
+            image: UiImage{
+                texture: button_materials,
+                flip_x: false,
+                flip_y: false,
+            },
+            style: Style {
+                size: Size::new(Val::Px(279.), Val::Px(76.)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..Default::default()
+        }, TwoVTwoGameButton));
+    });
 
-    // Create the buttons
-    commands.spawn((ButtonBundle {
-        image: UiImage{
-            texture: button_materials.clone(),
-            flip_x: false,
-            flip_y: false,
-        },
-        ..Default::default()
-    }, LocalGameButton)).push_children(&[node]);
-    commands.spawn((ButtonBundle {
-        image: UiImage{
-            texture: button_materials.clone(),
-            flip_x: false,
-            flip_y: false,
-        },
-        ..Default::default()
-    }, OneVOneGameButton)).push_children(&[node]);
-    commands.spawn((ButtonBundle {
-        image: UiImage{
-            texture: button_materials,
-            flip_x: false,
-            flip_y: false,
-        },
-        ..Default::default()
-    }, TwoVTwoGameButton)).push_children(&[node]);
 }
 
 fn button_system(
