34e4b6582d27dda48300129d2e0e3f675ff3c509
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-08 22:10:34 -0400

    aider: Added code to create a Bevy application with a camera, light source, and cube in the scene.
    # Context:
    USER: I added these *read-write* files: src/main.rs
    ASSISTANT: Great! Now we can start creating a simple 3D scene. We will create a new Bevy application, add a camera, a light source, and a 3D object (a cube) to the scene.
    
    Here is the code you need to add to your `src/main.rs` file:
    
    ```rust
    src/main.rs
    <<<<<<< ORIGINAL
    
    =======
    use bevy::prelude::*;
    
    fn main() {
        App::build()
            .add_plugins(DefaultPlugins)
            .add_startup_system(setup.system())
            .run();
    }
    
    fn setup(mut commands: Commands, mut materials: ResMut<StandardMaterial>) {
        // Add camera
        commands.spawn_bundle(PerspectiveCameraBundle {
            transform: Transform::from_xyz(3.0, 5.0, -8.0).looking_at(Vec3::ZERO, Vec3::Y),
            ..Default::default()
        });
    
        // Add light source
        commands.spawn_bundle(LightBundle {
            transform: Transform::from_xyz(4.0, 8.0, 4.0),
            ..Default::default()
        });
    
        // Add a cube
        commands.spawn_bundle(PbrBundle {
            mesh: Mesh::from(shape::Cube { size: 2.0 }),
            material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
            ..Default::default()
        });
    }
    >>>>>>> UPDATED
    ```
    
    This code creates a new Bevy application with the default plugins. The `setup` function is called at the start of the application. It adds a camera, a light source, and a cube to the scene. The cube is colored blue.

--- src/main.rs
+++ src/main.rs
@@ -91,3 +91,32 @@ fn setup(mut commands: Commands, mut materials: ResMut<StandardMaterial>) {
         ..Default::default()
     });
 }
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<StandardMaterial>) {
+    // Add camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(3.0, 5.0, -8.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    // Add light source
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+
+    // Add a cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: Mesh::from(shape::Cube { size: 2.0 }),
+        material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+        ..Default::default()
+    });
+}
