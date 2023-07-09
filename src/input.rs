f2256ff9d55c2ebf44160c6f4cc502c47d4ea7c5
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-09 02:10:37 -0400

    aider: Refactored the input module to use the correct API for gamepad input handling.
    # Context:
    USER: Refactor the input module to use the corrrect api based on this code https://bevy-cheatbook.github.io/input/gamepad.html?highlight=controller#gamepad-controller-joystick
    ASSISTANT: To refactor the input module to use the correct API, we need to modify the `src\input.rs` file. We will replace the current gamepad input handling with the correct API.
    
    Here are the changes:
    
    ```rust
    src\input.rs
    <<<<<<< ORIGINAL
    use bevy::prelude::*;
    use bevy_ggrs::ggrs::PlayerHandle;
    
    pub const INPUT_JUMP: u8 = 1 << 0;
    pub const INPUT_ATTACK: u8 = 1 << 1;
    pub const INPUT_BLOCK: u8 = 1 << 2;
    pub const INPUT_SPECIAL: u8 = 1 << 3;
    pub const INPUT_MOVE_X: u8 = 1 << 4;
    pub const INPUT_MOVE_Y: u8 = 1 << 5;
    pub const INPUT_MOVE_Z: u8 = 1 << 6;
    pub fn input(_: In<PlayerHandle>, gamepad: Res<Input<GamepadButton>>) -> u8 {
        let mut input = 0u8;
        if gamepad.just_pressed(GamepadButtonType::South) {
            input |= INPUT_JUMP;
        }
        if gamepad.just_pressed(GamepadButton::East) {
            input |= INPUT_ATTACK;
        }
        if gamepad.just_pressed(GamepadButton::West) {
            input |= INPUT_BLOCK;
        }
        if gamepad.just_pressed(GamepadButton::North) {
            input |= INPUT_SPECIAL;
        }
        if gamepad.axis_value(GamepadAxis::LeftStickX).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_X;
        }
        if gamepad.axis_value(GamepadAxis::LeftStickY).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_Y;
        }
        if gamepad.axis_value(GamepadAxis::RightStickY).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_Z;
        }
        input
    }
    =======
    use bevy::prelude::*;
    use bevy_ggrs::ggrs::PlayerHandle;
    
    pub const INPUT_JUMP: u8 = 1 << 0;
    pub const INPUT_ATTACK: u8 = 1 << 1;
    pub const INPUT_BLOCK: u8 = 1 << 2;
    pub const INPUT_SPECIAL: u8 = 1 << 3;
    pub const INPUT_MOVE_X: u8 = 1 << 4;
    pub const INPUT_MOVE_Y: u8 = 1 << 5;
    pub const INPUT_MOVE_Z: u8 = 1 << 6;
    pub fn input(_: In<PlayerHandle>, gamepad: Res<Input<Gamepad>>) -> u8 {
        let mut input = 0u8;
        if gamepad.button_pressed(GamepadButton::South(0)) {
            input |= INPUT_JUMP;
        }
        if gamepad.button_pressed(GamepadButton::East(0)) {
            input |= INPUT_ATTACK;
        }
        if gamepad.button_pressed(GamepadButton::West(0)) {
            input |= INPUT_BLOCK;
        }
        if gamepad.button_pressed(GamepadButton::North(0)) {
            input |= INPUT_SPECIAL;
        }
        if gamepad.axis_value(GamepadAxis::LeftStickX(0)).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_X;
        }
        if gamepad.axis_value(GamepadAxis::LeftStickY(0)).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_Y;
        }
        if gamepad.axis_value(GamepadAxis::RightStickY(0)).unwrap_or(0.0) > 0.0 {
            input |= INPUT_MOVE_Z;
        }
        input
    }
    >>>>>>> UPDATED
    ```
    
    This code replaces the current gamepad input handling with the correct API. The gamepad's South, East, West, and North buttons are used for jump, attack, block, and special actions respectively. The LeftStickX, LeftStickY, and RightStickY axes are used for moving in the X, Y, and Z directions respectively.

--- src/input.rs
+++ src/input.rs
@@ -8,27 +8,27 @@ pub const INPUT_SPECIAL: u8 = 1 << 3;
 pub const INPUT_MOVE_X: u8 = 1 << 4;
 pub const INPUT_MOVE_Y: u8 = 1 << 5;
 pub const INPUT_MOVE_Z: u8 = 1 << 6;
-pub fn input(_: In<PlayerHandle>, gamepad: Res<Input<GamepadButton>>) -> u8 {
+pub fn input(_: In<PlayerHandle>, gamepad: Res<Input<Gamepad>>) -> u8 {
     let mut input = 0u8;
-    if gamepad.just_pressed(GamepadButtonType::South) {
+    if gamepad.button_pressed(GamepadButton::South(0)) {
         input |= INPUT_JUMP;
     }
-    if gamepad.just_pressed(GamepadButton::East) {
+    if gamepad.button_pressed(GamepadButton::East(0)) {
         input |= INPUT_ATTACK;
     }
-    if gamepad.just_pressed(GamepadButton::West) {
+    if gamepad.button_pressed(GamepadButton::West(0)) {
         input |= INPUT_BLOCK;
     }
-    if gamepad.just_pressed(GamepadButton::North) {
+    if gamepad.button_pressed(GamepadButton::North(0)) {
         input |= INPUT_SPECIAL;
     }
-    if gamepad.axis_value(GamepadAxis::LeftStickX).unwrap_or(0.0) > 0.0 {
+    if gamepad.axis_value(GamepadAxis::LeftStickX(0)).unwrap_or(0.0) > 0.0 {
         input |= INPUT_MOVE_X;
     }
-    if gamepad.axis_value(GamepadAxis::LeftStickY).unwrap_or(0.0) > 0.0 {
+    if gamepad.axis_value(GamepadAxis::LeftStickY(0)).unwrap_or(0.0) > 0.0 {
         input |= INPUT_MOVE_Y;
     }
-    if gamepad.axis_value(GamepadAxis::RightStickY).unwrap_or(0.0) > 0.0 {
+    if gamepad.axis_value(GamepadAxis::RightStickY(0)).unwrap_or(0.0) > 0.0 {
         input |= INPUT_MOVE_Z;
     }
     input
