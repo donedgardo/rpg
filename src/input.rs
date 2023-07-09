bfb5a5e3f238c9875243733b7afc2eaaeaa123cd
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-09 00:50:06 -0400

    aider: Refactored the input handling code to handle inputs for a 2D fighter adventure game playable on a controller.
    # Context:
    USER: Lets refactor the inputs to instead now be that of a 2d fighter adventure type of game playble on controller
    ASSISTANT: To refactor the inputs for a 2D fighter adventure game playable on a controller, we would need to modify the input handling code. However, I see that the input handling code is in a read-only file `src\input.rs`.
    
    Could you please make this file read-write so that I can propose the necessary changes?
    USER: I added these *read-write* files: src/input.rs
    ASSISTANT: Great! Now, let's modify the `src/input.rs` file to handle inputs for a 2D fighter adventure game. We'll need to change the constants and the `input` function to handle new inputs such as jump, attack, block, and special. We'll also need to change the key bindings to match a typical game controller layout.
    
    Here are the changes:
    
    ```rust
    src/input.rs
    <<<<<<< ORIGINAL
    pub const INPUT_UP: u8 = 1 << 0;
    pub const INPUT_DOWN: u8 = 1 << 1;
    pub const INPUT_LEFT: u8 = 1 << 2;
    pub const INPUT_RIGHT: u8 = 1 << 3;
    pub const INPUT_FIRE: u8 = 1 << 4;
    =======
    pub const INPUT_JUMP: u8 = 1 << 0;
    pub const INPUT_ATTACK: u8 = 1 << 1;
    pub const INPUT_BLOCK: u8 = 1 << 2;
    pub const INPUT_SPECIAL: u8 = 1 << 3;
    pub const INPUT_LEFT: u8 = 1 << 4;
    pub const INPUT_RIGHT: u8 = 1 << 5;
    >>>>>>> UPDATED
    ```
    
    ```rust
    src/input.rs
    <<<<<<< ORIGINAL
    pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
        let mut input = 0u8;
        if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
            input |= INPUT_UP;
        }
        if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
            input |= INPUT_DOWN;
        }
        if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
            input |= INPUT_LEFT
        }
        if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
            input |= INPUT_RIGHT;
        }
        if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
            input |= INPUT_FIRE;
        }
        input
    }
    =======
    pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
        let mut input = 0u8;
        if keys.just_pressed(KeyCode::A) {
            input |= INPUT_JUMP;
        }
        if keys.just_pressed(KeyCode::B) {
            input |= INPUT_ATTACK;
        }
        if keys.just_pressed(KeyCode::X) {
            input |= INPUT_BLOCK;
        }
        if keys.just_pressed(KeyCode::Y) {
            input |= INPUT_SPECIAL;
        }
        if keys.pressed(KeyCode::Left) {
            input |= INPUT_LEFT;
        }
        if keys.pressed(KeyCode::Right) {
            input |= INPUT_RIGHT;
        }
        input
    }
    >>>>>>> UPDATED
    ```
    
    Please note that the key bindings are set to match a typical Xbox controller layout. You may need to adjust these to match your specific controller.

--- src/input.rs
+++ src/input.rs
@@ -1,28 +1,32 @@
 use bevy::prelude::*;
 use bevy_ggrs::ggrs::PlayerHandle;
 
-pub const INPUT_UP: u8 = 1 << 0;
-pub const INPUT_DOWN: u8 = 1 << 1;
-pub const INPUT_LEFT: u8 = 1 << 2;
-pub const INPUT_RIGHT: u8 = 1 << 3;
-pub const INPUT_FIRE: u8 = 1 << 4;
+pub const INPUT_JUMP: u8 = 1 << 0;
+pub const INPUT_ATTACK: u8 = 1 << 1;
+pub const INPUT_BLOCK: u8 = 1 << 2;
+pub const INPUT_SPECIAL: u8 = 1 << 3;
+pub const INPUT_LEFT: u8 = 1 << 4;
+pub const INPUT_RIGHT: u8 = 1 << 5;
 
 pub fn input(_: In<PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
     let mut input = 0u8;
-    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
-        input |= INPUT_UP;
+    if keys.just_pressed(KeyCode::A) {
+        input |= INPUT_JUMP;
     }
-    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
-        input |= INPUT_DOWN;
+    if keys.just_pressed(KeyCode::B) {
+        input |= INPUT_ATTACK;
     }
-    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
-        input |= INPUT_LEFT
+    if keys.just_pressed(KeyCode::X) {
+        input |= INPUT_BLOCK;
     }
-    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+    if keys.just_pressed(KeyCode::Y) {
+        input |= INPUT_SPECIAL;
+    }
+    if keys.pressed(KeyCode::Left) {
+        input |= INPUT_LEFT;
+    }
+    if keys.pressed(KeyCode::Right) {
         input |= INPUT_RIGHT;
-    }
-    if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
-        input |= INPUT_FIRE;
     }
     input
 }
