3d46dc3c4bb7a5a67647ab91eb58cd49ebbdc865
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-12 03:44:57 -0400

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use network::GgrsConfig;
 use network::NetworkPlugin;
 use menu::MenuPlugin;
 use bevy_editor_pls::prelude::*;
+use bevy_rapier2d::plugin::RapierPhysicsPlugin;
+use bevy_rapier2d::prelude::NoUserData;
+use bevy_rapier2d::render::RapierDebugRenderPlugin;
 use level::LevelPlugin;
 use gamepad::GamepadPlugin;
 
@@ -30,13 +33,18 @@ fn main() {
     app
         .add_state::<AppState>()
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugin(EditorPlugin::default())
         .add_plugin(CameraPlugin)
         .add_plugin(NetworkPlugin)
         .add_plugin(MenuPlugin)
         .add_plugin(LevelPlugin)
-        .add_plugin(GamepadPlugin)
-        .run();
+        .add_plugin(GamepadPlugin);
+    #[cfg(feature = "debug")]
+    {
+        app.add_plugin(RapierDebugRenderPlugin::default());
+    }
+    app.run();
 }
 
 // Time Energy
