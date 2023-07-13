0e36eb162563675f63bf6a146a5622e12298c619
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-07-13 02:03:02 -0400

    work in progress

--- src/level.rs
+++ src/level.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use bevy::prelude::*;
 use bevy::asset::Assets;
 use bevy::sprite::{MaterialMesh2dBundle, SpriteBundle};
@@ -7,6 +8,7 @@ use crate::player::{Player, Velocity};
 use bevy_ggrs::ggrs::{PlayerHandle};
 use bevy_ggrs::{RollbackIdProvider, PlayerInputs, Rollback, Session, GGRSSchedule};
 use nalgebra::ComplexField;
+use crate::gamepad::{GamepadAxes, MyGamepad};
 use crate::input::{INPUT_MOVE_DOWN, INPUT_MOVE_UP};
 use crate::network::GgrsConfig;
 
@@ -22,7 +24,7 @@ impl Plugin for LevelPlugin {
             .add_system(move_player_system.in_schedule(GGRSSchedule))
             .add_system(setup_scene.in_schedule(OnEnter(AppState::LocalPlay)))
             .add_system(spawn_local_players.in_schedule(OnEnter(AppState::LocalPlay)))
-            .add_system(move_local_player_system.in_schedule(GGRSSchedule));
+            .add_system(move_local_player_system.in_set(OnUpdate(AppState::LocalPlay)));
     }
 }
 
@@ -30,7 +32,6 @@ fn spawn_local_players(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut rip: ResMut<RollbackIdProvider>,
 ) {
     // Spawn a single player for local play
     commands.spawn((
@@ -44,26 +45,43 @@ fn spawn_local_players(
         },
         Player { handle: 0 },
         Velocity::default(),
-        rip.next(),
     ));
 }
 
 fn move_local_player_system(
-    mut query: Query<(&mut Velocity, &mut Transform, &Player), With<Rollback>>,
-    inputs: Res<Input<GamepadAxis>>,
+    mut query: Query<(&mut Velocity, &mut Transform, &Player)>,
+    my_gamepad: Option<Res<MyGamepad>>,
+    time: Res<Time>,
+    axes: Res<Axis<GamepadAxis>>,
 ) {
-    for (mut vel, mut transform, player) in query.iter_mut() {
-        let input = inputs.get(GamepadAxis::LeftStickX(player.handle));
-        let mut v = vel.0;
-        v.x += input * MOVEMENT_SPEED;
-        let mag = ComplexField::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-        if mag > MAX_SPEED {
-            let factor = MAX_SPEED / mag;
-            v.x *= factor;
-            v.y *= factor;
-            v.z *= factor;
+    let gamepad = if let Some(gp) = my_gamepad {
+        gp.0
+    } else {
+        return ;
+    };
+    let axis_lx = GamepadAxis {
+        gamepad,
+        axis_type: GamepadAxisType::LeftStickX,
+    };
+    for (mut vel, mut transform, _) in query.iter_mut() {
+        if let Some(x) = axes.get(axis_lx) {
+            let mut gamepad_axes = GamepadAxes {
+                lx: x,
+                ly: 0.,
+            };
+            gamepad_axes.apply_deadzone();
+            let mut v = vel.0;
+            // TODO make sure local and network run same
+            v.x += gamepad_axes.lx * MOVEMENT_SPEED * 100.;
+            let mag = ComplexField::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+            if mag > MAX_SPEED {
+                let factor = MAX_SPEED / mag;
+                v.x *= factor;
+                v.y *= factor;
+                v.z *= factor;
+            }
+            transform.translation += v * time.delta_seconds();
         }
-        transform.translation += v;
     }
 }
 
@@ -75,7 +93,6 @@ fn move_player_system(
         let input = inputs[player.handle].0;
         let mut v = vel.0;
         v.x += input.axis_lx * MOVEMENT_SPEED;
-        v.y += input.axis_ly * MOVEMENT_SPEED;
         let mag = ComplexField::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
         if mag > MAX_SPEED {
             let factor = MAX_SPEED / mag;
